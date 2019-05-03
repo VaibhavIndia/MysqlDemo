95ccf6dcd603aba36ea756439a3a547fc0ea948d
vaibhav <vaibhav.nigade@gmail.com>
2019-05-03 18:12:52 +0530

    additionprogram and commentprogram added

---
+++ src/java_practice/AdditionProgram.java
@@ -0,0 +1,13 @@
+package java_practice;
+
+public class AdditionProgram 
+{
+	public static void main(String[] args) 
+	{
+		// printing the addition of 2 numbers...
+		int a =4, b=43;
+		System.out.println(a+b);
+		
+	}
+
+}
---
+++ src/java_practice/Commentbasics.java
@@ -0,0 +1,16 @@
+package java_practice;
+
+public class Commentbasics 
+{
+	public static void main(String[] args) 
+	{
+		//  single line comment
+		System.out.println("above is single line comment");
+		
+		/*
+		 * Multiple line comment
+		 */
+		System.out.println("above is multiple line comment");
+	}
+
+}
