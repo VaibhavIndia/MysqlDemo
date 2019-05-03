43906e34aa6deb22a71a25d9115b61b56df9b548
vaibhav <vaibhav.nigade@gmail.com>
2019-05-03 17:56:19 +0530

    database program finished

--- src/structure/ConnectionCreation.java
+++ src/structure/ConnectionCreation.java
@@ -1,10 +1,36 @@
 package structure;
 
-public class ConnectionCreation {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class ConnectionCreation 
+{
+	public static void main(String[] args) throws SQLException 
+	{
+		// TODO Auto-generated method stub.
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		String host = "localhost";
+		String port = "3306";
+		Connection con = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/test2", "root", "");
+		Statement s = con.createStatement();
+		ResultSet rs = s.executeQuery("SELECT * FROM `data` where id = 1");
+		while (rs.next())	
+		{
+			System.out.println(rs.getString("id"));
+			System.out.println(rs.getString("name"));
+			System.out.println(rs.getString("email"));
+			System.out.println(rs.getString("gender"));
+			System.out.println(rs.getString("carid"));
+		}
+		
 	}
 
 }
