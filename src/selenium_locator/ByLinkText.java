1835d164a551a173cb4d3c243c0f3773cec7bc85
vaibhav <vaibhav.nigade@gmail.com>
2019-05-08 15:00:45 +0530

    new files added

--- src/selenium_locator/ByLinkText.java
+++ src/selenium_locator/ByLinkText.java
@@ -4,15 +4,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class UsingId {
+public class ByLinkText {
 
 	public static void main(String[] args) {
-		//Selenium Browser invoke code
-		
+		// TODO Auto-generated method stub
+
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Browsers\\chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
-		driver.findElement(By.id("email")).sendKeys("THIs is id");
+		driver.findElement(By.linkText("Forgotten account?")).click();;
 
 	}
 
--- src/selenium_locator/ReadymadeXpath.java
+++ src/selenium_locator/ReadymadeXpath.java
@@ -4,16 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class UsingId {
+public class ReadymadeXpath {
 
 	public static void main(String[] args) {
-		//Selenium Browser invoke code
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Browsers\\chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
-		driver.findElement(By.id("email")).sendKeys("THIs is id");
-
+		driver.findElement(By.xpath("//*[@id=\'email\']")).sendKeys("abc");
+		driver.findElement(By.xpath("//*[@id=\'pass\']")).sendKeys("abczxc");
+		driver.findElement(By.xpath("//*[@value=\'Log In\']")).click();
+		
 	}
 
 }
