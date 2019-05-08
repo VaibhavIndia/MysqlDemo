e445e81c96c4b24dd26697390f44e82e9b79e8e0
vaibhav <vaibhav.nigade@gmail.com>
2019-05-08 18:33:55 +0530

    comment added

--- src/selenium_locator/ByLinkText.java
+++ src/selenium_locator/ByLinkText.java
@@ -8,7 +8,7 @@ public class ByLinkText {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// in anchor tab
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Browsers\\chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
